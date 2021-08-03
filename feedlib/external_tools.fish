7a588d2ca8511d9dfe761ffd89225eb389f7c5b4
jl <jl@poltergeist>
2021-08-03 23:26:54 +0200

    .

--- feedlib/external_tools.fish
+++ feedlib/external_tools.fish
@@ -1,6 +1,8 @@
 #!/usr/bin/env fish
 
-# compdb, filter out phony targets
-ninja -f _build/debug/build.ninja -t compdb cxx exe | jq '[.[] | select(.command != "")]' > compile_commands.json
+set BUILD_DIR _build
 
-clang-tidy-13 -p . --extra-arg="-isystem/usr/lib/llvm-13/include/c++/v1" src/feedlib.cpp 2>&1 | tee _build/clang-tidy.log
+# compdb, filter out phony targets
+ninja -f $BUILD_DIR/debug/build.ninja -t compdb cxx exe | jq '[.[] | select(.command != "")]' > compile_commands.json
+
+clang-tidy-13 -p . --extra-arg="-isystem/usr/lib/llvm-13/include/c++/v1" src/feedlib.cpp 2>&1 | tee $BUILD_DIR/clang-tidy.log
